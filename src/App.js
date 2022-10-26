3de480c83607dc6934bf0de3b17b36ecdcf29ef9
MauLight <maulisseluz@gmail.com>
2022-10-26 02:42:15 -0300

    second commit, display fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import {Cloudinary} from "@cloudinary/url-gen";
-import {AdvancedImage} from '@cloudinary/react';
-import {fill} from '@cloudinary/url-gen/actions/resize';
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from '@cloudinary/react';
+import { fill } from '@cloudinary/url-gen/actions/resize';
 
 
 const App = () => {
+
+  const style = {
+    width: '18rem'
+  }
+
   const cld = new Cloudinary({  // Create a Cloudinary instance and set your cloud name.
     cloud: {
       cloudName: 'maulight'
     }
   });
 
-  const myImage= cld.image('cld-sample-3.jpg');
+  const myImage = cld.image('cld-sample-3.jpg');
   myImage.resize(fill().width(350).height(350));
 
   return (
-    <div>
-      <AdvancedImage cldImg= {myImage} />
+    <div className="card m-auto" style={style}>
+      <AdvancedImage className="card-img-top" cldImg={myImage} />
+      <div className="card-body">
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
     </div>
   )
 
 };
 
 export default App;
+
